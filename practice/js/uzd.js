0142311517a5788452825c41c0851c499f57d0b4
Marius <marius@example.com>
2022-10-19 13:47:58 +0300

    sdsd

---
+++ practice/js/uzd.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('uzd.js');
+
+/*  
+// prisideti elementam id, class, ir pan pagal poreiki
+// panaudoti tiek getElementById tiek querySelector() ar querySelectorAll()
+
+1. pakeisti h1 tektso spalva i tomato <h1>Welcome</h1>
+
+2. padaryti kad fonto storis butu normalus  <h2>Some title</h2>
+
+3. <p>Lorem ipsum dolor si. Irasyti i ji teksta "man pavyko dinamiskai atnaujinti teksta"
+
+4. <li>two</li> padidinti srifto dyti iki 34px
+
+*/
